e27f769e96e2c1366812b03eda94bad92daded4f
myrdin <myrdin@protonmail.com>
2019-08-27 00:45:56 +0200

    WIP slideshow

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,21 @@ function toggleMenu() {
     }
 }
 
-// Show/hide hover imgs
+// Slide effect
 
-function show() {
-    document.getElementById('text-1').style.display = "block";
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
 }
-
-function hide() {
-    document.getElementById('text-1').style.display = "none";
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slider");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
 }
