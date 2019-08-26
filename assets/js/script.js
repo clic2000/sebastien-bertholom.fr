a0e4b804dbf609ed2ff42f73ee71a7f9d2939dd1
myrdin <myrdin@protonmail.com>
2019-08-26 22:32:31 +0200

    Toggling the menu

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Animated Burger
+function toggleBurger(x) {
+    x.classList.toggle("change");
+}
+
+// Menu toggle
+var menu = document.getElementById("menu");
+menu.style.display = "none";
+
+function toggleMenu() {
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
