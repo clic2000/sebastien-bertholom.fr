a6cc3399598b9a5b97982b85e9d36826c3f7d1ff
Mathilde Blanchemanche <mathilde@clic2000.fr>
2020-04-01 13:37:14 +0000

    Update 'assets/js/tracking.js'
    update Matomo domain

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -3,7 +3,7 @@ var _paq = window._paq || [];
   _paq.push(['trackPageView']);			
   _paq.push(['enableLinkTracking']);			
   (function() {			
-    var u="https://stats.clic2000.net/";			
+    var u="https://stats.clic2000.fr/";			
     _paq.push(['setTrackerUrl', u+'matomo.php']);			
     _paq.push(['setSiteId', '3']);			
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];			
